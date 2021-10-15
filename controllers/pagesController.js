bab1fde489a277e1501172969becfbdd683f62cb
Neri Marset <nerimarset@gmail.com>
2021-10-15 12:20:16 -0300

    12:15

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -11,42 +11,34 @@ async function showRegister(req, res) {
   res.send("Esto es el Login.");
 }
 
-// Cara los Tweets de los following
+// Muestra los Tweets de los following
 async function showHome(req, res) {
   console.log(req.user);
   try {
-    const homeTweets = await Tweet.find({ owner: { $in: [...req.user.following] } })
+    const thisUser = await User.findOne({ id: req.user.id });
+    const homeTweets = await Tweet.find({ owner: { $in: [...req.user.following, req.user] } })
       .limit(20)
-      .sort("date")
+      .sort({ date: "desc" })
       .populate("owner");
 
-    res.render("home", { homeTweets });
-  } catch (error) {
-    console.log(error);
-  }
-}
-<<<<<<< HEAD
-
-// Cara Los Tweets del User logeado
-async function showMyProfile(req, res) {
-  try {
-    const homeTweets = await Tweet.find({ owner: { $in: [req.user] } })
-      .limit(20)
-      .sort("date")
-      .populate("owner");
-
-    res.render("profile", { homeTweets });
+    res.render("home", { homeTweets, thisUser });
   } catch (error) {
     console.log(error);
   }
 }
 
-=======
->>>>>>> 1e9b9b8db305a31d85c49bc97532d8a4f26ce104
+//Muestra los Tweets del usuario
 async function showProfile(req, res) {
-  const thisUser = await User.findOne({ userName: req.params.userName });
-
-  res.render("profile", { thisUser });
+  try {
+    const thisUser = await User.findOne({ userName: req.params.userName });
+    const profileTweets = await Tweet.find({ owner: { $in: [req.user] } })
+      .limit(20)
+      .sort({ date: "desc" })
+      .populate("owner");
+    res.render("profile", { thisUser, profileTweets });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
