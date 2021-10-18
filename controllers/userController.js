2404f3b8f5120725976d220be7994cf2154763c8
joaquinareal <90458336+joaquinareal@users.noreply.github.com>
2021-10-18 10:09:52 -0300

    Revert "Merge branch 'main' of https://github.com/vitricida/HA_Twitter"
    This reverts commit 3caa18fa447a0362950070af9ed081a6a445bcb0, reversing
    changes made to ea4479721564b3cde25355efb626b131652364bf.

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,17 +11,6 @@ async function register(req, res) {
   }
 }
 
-async function searchUser(req, res) {
-  console.log("aca llegue");
-  console.log(req.query);
-  const lookFor = String(req.query.lookFor);
-  const result = await User.find({
-    $or: [{ userName: lookFor }, { firtsName: lookFor }, { lastName: lookFor }],
-  });
-  console.log(result);
-}
-
 module.exports = {
   register,
-  searchUser,
 };
