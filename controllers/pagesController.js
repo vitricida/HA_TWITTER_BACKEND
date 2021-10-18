e2629897d5d47a9f546223259ff213a46eed1b06
Neri Marset <nerimarset@gmail.com>
2021-10-18 09:53:08 -0300

    revertí lo de rigthSideBar. está la vista de users

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -5,31 +5,10 @@ async function showIndex(req, res) {
   res.render("root");
 }
 
-/*async function showProfile(req, res) {
-  const user = await User.findById(req.user._id);
-  const users = await User.find({ _id: { $ne: req.user._id } });
-  const ids = [];
-  for (member of users) ids.push(member._id);
-  const shuffled = ids.sort(function () {
-    return 0.5 - Math.random();
-  });
-  const selected = shuffled.slice(0, 3);
-  const randomUsers = [];
-
-  for (id of selected) {
-    const user = await User.findById(id);
-    randomUsers.push(user);
-  }
-  
-
-  res.render("profile", { user, randomUsers });
-}
-*/
-
 //Mostar Usuarios
 async function showUser(req, res) {
   try {
-    const users = await User.find({ _id: { $ne: req.user._id } });
+    const users = await User.find().limit(20);
     console.log(users);
 
     res.render("users", { users });
@@ -40,32 +19,18 @@ async function showUser(req, res) {
 
 // Muestra los Tweets de los following
 async function showHome(req, res) {
-  //console.log(req.user);
+  console.log(req.user);
   try {
-    const user = await User.findById(req.user._id);
-    const users = await User.find({ _id: { $ne: req.user._id } });
     const thisUser = await User.findOne({ id: req.user.id });
     const homeTweets = await Tweet.find({ owner: { $in: [...req.user.following, req.user] } })
       .limit(20)
       .sort({ date: "desc" })
       .populate("owner");
     //.populate("likes");
-    const ids = [];
-    for (member of users) ids.push(member._id);
-    const shuffled = ids.sort(function () {
-      return 0.5 - Math.random();
-    });
-    const selected = shuffled.slice(0, 3);
-    const randomUsers = [];
-    for (id of selected) {
-      const user = await User.findById(id);
-      randomUsers.push(user);
-    }
+    const users = await User.find().limit(4); //Error en esta línea
+    console.log(users);
 
-    //const users = await User.find().limit(4); //Error en esta línea
-    console.log(randomUsers);
-
-    res.render("home", { homeTweets, thisUser, users, randomUsers });
+    res.render("home", { homeTweets, thisUser, users });
   } catch (error) {
     console.log(error);
   }
