7255a96c86d0c2036f1f556db995874435484c39
vitricida <pierre.poumayrac@gmail.com>
2021-11-05 14:09:48 -0300

    .

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -6,13 +6,6 @@ const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
 const tweetController = require("../controllers/tweetController");
 
-const validateRegistrationData = require("../middlewares/validateRegistrationData");
-const redirectToHomeIfLoggedIn = require("../middlewares/redirectToHomeIfLoggedIn");
-const isLoggedIn = require("../middlewares/isLoggedIn");
-
-//publicRouter.get("/users", pagesController.showUser);
-
-//publicRouter.get("/home", isLoggedIn, pagesController.showHome);
 publicRouter.get(
   "/tweets",
   checkJwt({ secret: process.env.SECRET, algorithms: ["HS256"] }),
@@ -41,14 +34,7 @@ publicRouter.get(
   pagesController.randomUsers,
 );
 
-//publicRouter.get("/profile/:userName", isLoggedIn, pagesController.showProfile);
 publicRouter.post("/login", authController.logIn);
 publicRouter.post("/register", userController.register);
-publicRouter.get("/:userName", pagesController.showProfile);
-
-//publicRouter.post("/register", validateRegistrationData, userController.register);
-publicRouter.get("/searchUsers", userController.searchUser);
-
-//publicRouter.post("/follow", userController.followToggle);
 
 module.exports = publicRouter;
